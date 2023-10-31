7a67e1480e76dd0a4074368368c4825cd5e9758d
Rahul Verma <86731857+rahulverma6zii@users.noreply.github.com>
2023-10-31 12:37:54 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let mybutton = document.getElementById("myBtn");
 let insight_buttons = document.getElementsByClassName("insight-buttons");
 let navbar = document.getElementsByClassName("navbar")[0];
 let img = document.getElementsByTagName("img");
-
+let currentYear=document.getElementsByClassName("currentYear");
 let insights_items = [
   {
     image: "https://img.freepik.com/free-photo/indian-businessman-with-his-white-car_496169-2889.jpg?w=360&t=st=1698077702~exp=1698078302~hmac=75006da43490a9194a278e68fc4d960975c45dc707ec5920e6d91ad0e247e3b2",
@@ -169,3 +169,4 @@ function updateCarousel() {
   })
 }
 updateCarousel();
+currentYear[0].innerHTML=`©${new Date().getFullYear()} CREDWEL`;
